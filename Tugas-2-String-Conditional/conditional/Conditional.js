68fca7c8aee56f84580e646b443f9c35133ac129
Supriadi <supriadi15001@gmail.com>
2021-09-01 07:21:40 +0700

    Mengerjakan tugas string dan conditional

---
+++ Tugas-2-String-Conditional/conditional/Conditional.js
@@ -0,0 +1,171 @@
+// Soal Nomor 1
+
+var nama = '';
+var peran = 'penyihir';
+
+if (nama == '') {
+    console.log('"Nama harus diisi!"');
+} else {
+    if (peran == '') {
+        console.log('"' + 'Halo ' + nama + ', Pilih peranmu untuk memulai game!' + '"');
+    } else if (peran == 'Penyihir') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu dapat melihat siapa yang menjadi werewolf!' + '"');
+    } else if (peran == 'Guard') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu akan membantu melindungi temanmu dari serangan werewolf.' + '"');
+    } else if (peran == 'Werewolf') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', Kamu akan memakan mangsa setiap malam!' + '"');
+    }
+}
+
+
+
+var nama = 'John';
+var peran = '';
+
+if (nama == '') {
+    console.log('"Nama harus diisi!"');
+} else {
+    if (peran == '') {
+        console.log('"' + 'Halo ' + nama + ', Pilih peranmu untuk memulai game!' + '"');
+    } else if (peran == 'Penyihir') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu dapat melihat siapa yang menjadi werewolf!' + '"');
+    } else if (peran == 'Guard') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu akan membantu melindungi temanmu dari serangan werewolf.' + '"');
+    } else if (peran == 'Werewolf') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', Kamu akan memakan mangsa setiap malam!' + '"');
+    }
+}
+
+
+
+var nama = 'Jane';
+var peran = 'Penyihir';
+
+if (nama == '') {
+    console.log('"Nama harus diisi!"');
+} else {
+    if (peran == '') {
+        console.log('"' + 'Halo ' + nama + ', Pilih peranmu untuk memulai game!' + '"');
+    } else if (peran == 'Penyihir') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu dapat melihat siapa yang menjadi werewolf!' + '"');
+    } else if (peran == 'Guard') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu akan membantu melindungi temanmu dari serangan werewolf.' + '"');
+    } else if (peran == 'Werewolf') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', Kamu akan memakan mangsa setiap malam!' + '"');
+    }
+}
+ 
+
+
+var nama = 'Jenita';
+var peran = 'Guard';
+
+if (nama == '') {
+    console.log('"Nama harus diisi!"');
+} else {
+    if (peran == '') {
+        console.log('"' + 'Halo ' + nama + ', Pilih peranmu untuk memulai game!' + '"');
+    } else if (peran == 'Penyihir') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu dapat melihat siapa yang menjadi werewolf!' + '"');
+    } else if (peran == 'Guard') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu akan membantu melindungi temanmu dari serangan werewolf.' + '"');
+    } else if (peran == 'Werewolf') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', Kamu akan memakan mangsa setiap malam!' + '"');
+    }
+}
+
+
+
+var nama = 'Junaedi';
+var peran = 'Werewolf';
+
+if (nama == '') {
+    console.log('"Nama harus diisi!"');
+} else {
+    if (peran == '') {
+        console.log('"' + 'Halo ' + nama + ', Pilih peranmu untuk memulai game!' + '"');
+    } else if (peran == 'Penyihir') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu dapat melihat siapa yang menjadi werewolf!' + '"');
+    } else if (peran == 'Guard') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', kamu akan membantu melindungi temanmu dari serangan werewolf.' + '"');
+    } else if (peran == 'Werewolf') {
+        console.log('"' + 'Selamat datang di Dunia Werewolf, ' + nama + '"');
+        console.log('"' + 'Halo ' + peran + ' ' + nama 
+            + ', Kamu akan memakan mangsa setiap malam!' + '"');
+    }
+}
+
+
+
+// Soal Nomor 2
+
+var hari = 21;
+var bulan = 1;
+var tahun = 1945;
+
+switch (bulan) {
+    case 1: 
+        console.log(hari + ' Januari ' + tahun); 
+        break;
+    case 2: 
+        console.log(hari + ' Februari ' + tahun); 
+        break;
+    case 3: 
+        console.log(hari + ' Maret ' + tahun); 
+        break;
+    case 4: 
+        console.log(hari + ' April ' + tahun); 
+        break;
+    case 5: 
+        console.log(hari + ' Mei ' + tahun); 
+        break;
+    case 6: 
+        console.log(hari + ' Juni ' + tahun); 
+        break;
+    case 7: 
+        console.log(hari + ' Juli ' + tahun); 
+        break;
+    case 8: 
+        console.log(hari + ' Agustus ' + tahun); 
+        break;
+    case 9: 
+        console.log(hari + ' September ' + tahun); 
+        break;
+    case 10: 
+        console.log(hari + ' Oktober ' + tahun); 
+        break;
+    case 11: 
+        console.log(hari + ' November ' + tahun); 
+        break;
+    case 12: 
+        console.log(hari + ' Desember ' + tahun); 
+        break;
+}
